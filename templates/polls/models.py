f77e30b8189196e0ca73b805ce0162e64993cc83
zhen li <zhljen@gmail.com>
2015-11-25 15:56:13 -0800

    First site

---
+++ templates/polls/models.py
@@ -0,0 +1,22 @@
+import datetime
+from django.db import models
+from django.utils import timezone
+
+
+# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_descrption = 'Published recently?'
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
